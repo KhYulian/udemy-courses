e0476ddc9ebec8688bd5d77fe2eca72a6fc1250d
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-21 12:53:01 +0300

    passing data via props

--- React course/React Basics and working with components/first-react-app/src/App.js
+++ React course/React Basics and working with components/first-react-app/src/App.js
@@ -1,13 +1,53 @@
 // Each component should be in a separate file which we import into App.js
 import ExpenseItem from './components/ExpenseItem';
 
+function createDate() {
+	return new Date().toDateString().split(' ').slice(1).join(' ');
+}
+
 function App() {
+	const expenses = [
+		{
+			title: 'Car insuranse',
+			amount: 345.52,
+			date: createDate()
+		},
+		{
+			title: 'Food',
+			amount: 552.67,
+			date: createDate()
+		},
+		{
+			title: 'Rent',
+			amount: 2672.34,
+			date: createDate()
+		},
+		{
+			title: 'New Computer',
+			amount: 1120.99,
+			date: createDate()
+		}
+	];
 	// React uses declarative approach
 	return (
 		<div className="App">
 			<header className="App-header">Let's get started</header>
 			{/* We use components as HTML elements */}
-			<ExpenseItem></ExpenseItem>
+			<ExpenseItem
+				title={expenses[0].title}
+				amount={expenses[0].amount}
+				date={expenses[0].date}
+			></ExpenseItem>
+			<ExpenseItem
+				title={expenses[1].title}
+				amount={expenses[1].amount}
+				date={expenses[1].date}
+			></ExpenseItem>
+			<ExpenseItem
+				title={expenses[2].title}
+				amount={expenses[2].amount}
+				date={expenses[2].date}
+			></ExpenseItem>
 		</div>
 	);
 }
--- React course/React Basics and working with components/first-react-app/src/components/ExpenseItem.js
+++ React course/React Basics and working with components/first-react-app/src/components/ExpenseItem.js
@@ -4,20 +4,19 @@ import './ExpenseItem.css';
 // Repeat the filename in the name of function
 
 // A component in React is just a JavaScript function
-function ExpenseItem() {
-	const expenseDate = new Date().toDateString().split(' ').slice(1).join(' ');
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 345.56;
+function ExpenseItem(props) {
+	// we will get one parameter in any component we use. It will be an object which holds all recieved attribues as properties(props)
+
 	// return jsx code
 	return (
 		// Wrap HTML code into parentheses
 		// We can return only one root element
 		<div className="expense-item">
 			{/* you can run basic JavaScript expressions inside curly braces */}
-			<div> {expenseDate} </div>
+			<div> {props.date} </div>
 			<div className="expense-item__description">
-				<h2>{expenseTitle}</h2>
-				<div className="expense-item__price">{expenseAmount}$</div>
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">{props.amount}$</div>
 			</div>
 		</div>
 	);
