321e0af64084ce9c7c05137e60c8869ef1a41a75
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-21 11:03:14 +0300

    Starting React project

---
+++ React course/React Basics and working with components/first-react-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Let's get started</header>
+			<p>This is also visible</p>
+			<p>That's how we work with component</p>
+			<p>React uses declarative approach. We define the desired end state.</p>
+		</div>
+	);
+}
+
+export default App;
