f969ef3f23379a30cec234f12552b1854d661a3c
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-23 11:26:56 +0300

    working with state: useState

--- React course/React Basics and working with components/first-react-app/src/components/Expenses/ExpenseItem.js
+++ React course/React Basics and working with components/first-react-app/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,6 @@
+// useState is a React hook
+import { useState } from 'react';
+
 // Import css to use in component
 import './ExpenseItem.css';
 
@@ -10,9 +13,13 @@ import ExpenseDate from './ExpenseDate';
 // A component in React is just a JavaScript function
 function ExpenseItem(props) {
 	// we will get one parameter in any component we use. It will be an object which holds all recieved attribues as properties(props)
+
+	// useState returns array. First value is value itself. Second element is update function. Name funciton with set prefix
+	const [title, setTitle] = useState(props.title);
+
 	function clickHandler() {
 		// Name function like this if they triggered with event.
-		console.log('Clicked!');
+		setTitle('New Title');
 	}
 	// return jsx code
 	return (
@@ -22,7 +29,7 @@ function ExpenseItem(props) {
 			{/* you can run basic JavaScript expressions inside curly braces */}
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">{props.amount}$</div>
 			</div>
 			<button onClick={clickHandler}>Change Title</button>
