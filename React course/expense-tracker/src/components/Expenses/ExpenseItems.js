c7f713fa74d8a814ea64f965fd6f374b4afcba51
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-23 22:01:34 +0300

    Adding conditional return statements

--- React course/expense-tracker/src/components/Expenses/ExpenseItems.js
+++ React course/expense-tracker/src/components/Expenses/ExpenseItems.js
@@ -4,6 +4,7 @@ import './ExpenseItems.css';
 
 import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 
 function ExpenseItems(props) {
 	const { expenses } = props;
@@ -18,47 +19,13 @@ function ExpenseItems(props) {
 		return expense.date.getFullYear() + '' === filterYear;
 	});
 
-	let expensesContent = <h3>No expenses found</h3>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map(item => (
-			<ExpenseItem
-				key={item.id}
-				title={item.title}
-				amount={item.amount}
-				date={item.date}
-			/>
-		));
-	}
-
 	return (
 		<div className="expense-items">
 			<ExpenseFilter
 				defaultYear={filterYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{expensesContent}
-
-			{/* <ExpenseItem
-				title={expenses[0].title}
-				amount={expenses[0].amount}
-				date={expenses[0].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[1].title}
-				amount={expenses[1].amount}
-				date={expenses[1].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[2].title}
-				amount={expenses[2].amount}
-				date={expenses[2].date}
-			></ExpenseItem>
-			<ExpenseItem
-				title={expenses[3].title}
-				amount={expenses[3].amount}
-				date={expenses[3].date}
-			></ExpenseItem> */}
+			<ExpensesList expenses={filteredExpenses} />		
 		</div>
 	);
 }
