a6b2057717c4bb45d7efd45a79a6491fdb83e6f1
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-23 14:43:01 +0300

    listening to User Input

--- React course/expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ React course/expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,16 @@
 import './ExpenseForm.css';
 
 function ExpenseForm(props) {
+	function titleChangeHandler(event) {
+		console.log(event.target.value);
+	}
+
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label for="title">Title</label>
-					<input type="text" id="title" />
+					<input onChange={titleChangeHandler} type="text" id="title" />
 				</div>
 				<div className="new-expense__control">
 					<label for="amount">Amount</label>
@@ -17,7 +21,7 @@ function ExpenseForm(props) {
 					<input type="date" min="2018-01-01" max="22-12-31" id="date" />
 				</div>
 			</div>
-			
+
 			<div className="new-expense__actions">
 				<button type="submit">Add Expense</button>
 			</div>
