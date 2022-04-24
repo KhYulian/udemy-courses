4dd875e689f278fe33237d05b89b2d9274c6f9ac
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-24 22:12:17 +0300

    Styled-components

--- React course/styling-react-components/src/components/UI/Button/Button.js
+++ React course/styling-react-components/src/components/UI/Button/Button.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+const Button = styled.button`
+	font: inherit;
+	padding: 0.5rem 1.5rem;
+	border: 1px solid #03396c;
+	color: white;
+	background: #03396c;
+	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+	cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+	&:focus {
+		outline: none;
+	}
+
+	&:hover,
+	&:active {
+		background: #005b96;
+		border-color: #005b96;
+		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+	}
+`;
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
