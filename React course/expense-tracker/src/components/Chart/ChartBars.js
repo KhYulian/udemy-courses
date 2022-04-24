aaa7524316199bddb7bc500db5ad5123addb0997
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-24 19:12:21 +0300

    Adding Dynamic Styles

--- React course/expense-tracker/src/components/Chart/ChartBars.js
+++ React course/expense-tracker/src/components/Chart/ChartBars.js
@@ -1,7 +1,19 @@
 import './ChartBar.css';
 
 function ChartBar(props) {
-    
+	let barFillHight = '0%';
+
+	if (props.max > 0)
+		barFillHight = Math.round((props.value / props.max) * 100) + '%';
+
+	return (
+		<div className="chart-bar">
+			<div className="chart-bar__inner">
+				<div className="chart-bar__fill" style={{ height: barFillHight }}></div>
+			</div>
+			<div className="chart-bar__label">{props.label}</div>
+		</div>
+	);
 }
 
 export default ChartBar;
