c9ec243f7e9437e3b40f123e1a66b33be490b2c5
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-28 11:58:59 +0300

    Building and using a custom context provider component

--- React course/side-effects-reducers-context/src/components/Home/Home.js
+++ React course/side-effects-reducers-context/src/components/Home/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../store/authContex';
+import Button from '../UI/Button/Button';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
-const Home = (props) => {
-  return (
-    <Card className={classes.home}>
-      <h1>Welcome back!</h1>
-    </Card>
-  );
+const Home = props => {
+	const authContext = useContext(AuthContext);
+
+	return (
+		<Card className={classes.home}>
+			<h1>Welcome back!</h1>
+			<Button onClick={authContext.onLogout}>Logout</Button>
+		</Card>
+	);
 };
 
 export default Home;
