e3ed701dd12ce1dceaec1fd9f4ff2c508d2ae444
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-04-30 11:31:07 +0300

    Working with Refs and Forward Reft

--- React course/food-order-app/src/Components/Meals/MealItem/MealItem.js
+++ React course/food-order-app/src/Components/Meals/MealItem/MealItem.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './MealItem.module.css';
 
+import CartContext from '../../../store/cart-contex';
 import MealItemForm from './MealItemForm';
 
 const MealItem = props => {
+	const cartContex = useContext(CartContext);
+
 	const price = `$${props.price.toFixed(2)}`;
+	const addToCartHandler = amount => {
+		cartContex.addItem({
+			id: props.id,
+			name: props.name,
+			amount: amount, 
+			price: props.price
+		})
+	};
 
 	return (
 		<li className={classes.meal}>
@@ -15,7 +26,7 @@ const MealItem = props => {
 				<div className={classes.price}>{price}</div>
 			</div>
 			<div>
-				<MealItemForm id={props.id} />
+				<MealItemForm id={props.id} onAddToCart={addToCartHandler} />
 			</div>
 		</li>
 	);
