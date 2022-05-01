448b6a08d6fd632e54a1f962a152976e25bc5936
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-05-01 11:02:38 +0300

    Using the useEffect hook to animate button

--- React course/food-order-app/src/Components/Layout/Header/HeaderCartButton.js
+++ React course/food-order-app/src/Components/Layout/Header/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import classes from './HeaderCartButton.module.css';
 
@@ -7,13 +7,30 @@ import CartIcon from './CartIcon';
 
 const HeaderCartButton = props => {
 	const cartContext = useContext(CartContext);
+	const { items } = cartContext;
 
-	const numberOfCartItems = cartContext.items.reduce((cur, item) => {
+	const numberOfCartItems = items.reduce((cur, item) => {
 		return cur + item.amount;
 	}, 0);
+	const [btnIsAnimated, setBtnIsAnimated] = useState(false);
+
+	const btnClasses = `${classes.button} ${btnIsAnimated && classes.bump}`;
+
+	useEffect(() => {
+		if (items.length === 0) return;
+		setBtnIsAnimated(true);
+
+		const timer = setTimeout(
+			() => setBtnIsAnimated(false),
+
+			300
+		);
+
+		return () => clearTimeout(timer);
+	}, [items]);
 
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
