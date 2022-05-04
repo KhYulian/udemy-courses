6a0c63b46a893c58483d2f0d1e11154151abf0e6
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-05-04 20:46:54 +0300

    Handling Loading and Data States

--- React course/http-requests/src/App.js
+++ React course/http-requests/src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 
 function App() {
 	const [movies, setmovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const fetchMoviesHandler = async () => {
 		try {
+			setIsLoading(true);
 			const response = await fetch('https://swapi.dev/api/films/', {
 				method: 'GET'
 			});
@@ -20,8 +22,8 @@ function App() {
 					releaseDate: el.release_date
 				};
 			});
-
 			setmovies(transformedMovies);
+			setIsLoading(false);
 		} catch (error) {}
 	};
 
@@ -31,7 +33,10 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && <p>No movies found.</p>}
+				{isLoading && <p>Loading...</p>}
+				{}
 			</section>
 		</React.Fragment>
 	);
