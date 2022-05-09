76a675ef1a9c32891ccc97e175f17b31c2341e4b
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-05-09 20:28:40 +0300

    Managing cart

--- React course/redux-advanced/src/components/Shop/ProductItem.js
+++ React course/redux-advanced/src/components/Shop/ProductItem.js
@@ -1,23 +1,39 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
-const ProductItem = (props) => {
-  const { title, price, description } = props;
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart';
 
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+const ProductItem = props => {
+	const dispatch = useDispatch();
+
+	const { title, price, description, id } = props;
+
+	const productAddedToCartHandler = () => {
+		dispatch(
+			cartActions.addItemToCart({
+				id,
+				title,
+				price,
+				description
+			})
+		);
+	};
+
+	return (
+		<li className={classes.item} key={`${id}p`}>
+			<Card>
+				<header>
+					<h3>{title}</h3>
+					<div className={classes.price}>${price.toFixed(2)}</div>
+				</header>
+				<p>{description}</p>
+				<div className={classes.actions}>
+					<button onClick={productAddedToCartHandler}>Add to Cart</button>
+				</div>
+			</Card>
+		</li>
+	);
 };
 
 export default ProductItem;
