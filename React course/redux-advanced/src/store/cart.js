10077dce89fa93609ffab9a11749d4fae4eac819
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-05-09 19:40:50 +0300

    Working with cartSlice. Added reducer

---
+++ React course/redux-advanced/src/store/cart.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	items: [],
+	totalQuantity: 0
+};
+
+const cartSlice = createSlice({
+	name: 'cart',
+	initialState,
+	reducers: {
+		addItemToCart(state, action) {
+			const newItem = action.payload;
+			const existingItem = state.items.find(item => item.id === newItem.id);
+			if (!existingItem) {
+				state.items.push({
+					itemId: newItem.id,
+					price: newItem.price,
+					quantity: 1,
+					totalPrice: newItem.price,
+					title: newItem.title
+				}); // dont mutate in regular redux
+			} else {
+				// Item does exist - update existing item
+				existingItem.quantity++;
+				existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+			}
+		},
+		removeItemFromCart(state, action) {}
+	}
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
