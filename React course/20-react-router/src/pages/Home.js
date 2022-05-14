adf6b4a5d00bedada50974f81cc987e88e7ab188
Yulian Khomechko <khomechkoyulian@gmail.com>
2022-05-14 18:08:12 +0300

    Nested routes

--- React course/20-react-router/src/pages/Home.js
+++ React course/20-react-router/src/pages/Home.js
@@ -1,3 +1,12 @@
+import { Route } from 'react-router-dom';
+
 export default function Home() {
-	return <h1>The Home Page</h1>;
+	return (
+		<section>
+			<h1>The Home Page</h1>
+			<Route path="/home/new-user">
+				<h1>Welcome, new user</h1>
+			</Route>
+		</section>
+	);
 }
